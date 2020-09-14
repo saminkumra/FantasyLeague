2aab93449b79ceae686d5bc214e9f2ea827678af
Samin Kumra <saminkumra@gmail.com>
2020-09-14 22:40:21 +0530

    team class setup

--- team.py
+++ team.py
@@ -1,3 +1,4 @@
 
 class Team:
-    def __init__(self, id, name, played, points, position, strength_overall_away, strength_overall_home):
+    def __init__(self, id, name, played, points, position, strength_attack_away, strength_attack_home,
+                 strength_defence_away, strength_defence_home, strength_overall_away, strength_overall_home):
