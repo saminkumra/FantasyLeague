7083716aac4fc28e456061ca453524142370f1f2
Samin Kumra <saminkumra@gmail.com>
2020-09-14 22:24:28 +0530

    player class setup

---
+++ player.py
@@ -0,0 +1,5 @@
+
+class Player:
+    def __init__(self, ID, first_name, second_name, team, position, current_price, points, minutes,
+                 goals_scored, assists, clean_sheets, goals_conceded, own_goals, penalties_saved, penalties_missed,
+                 yellow_cards, red_cards, saves, bonus_points):
