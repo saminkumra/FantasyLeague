74eb35669b9ae7ff7704630a675f6a59f11c7c45
Samin Kumra <saminkumra@gmail.com>
2020-09-14 18:27:26 +0530

    just a test - using the API

--- test.py
+++ test.py
@@ -13,3 +13,7 @@ teams_df = pd.DataFrame(json_obj['teams'])
 slim_elements_df = elements_df[['second_name','team','element_type','selected_by_percent','now_cost','minutes',
                                 'transfers_in','value_season','total_points']]
 print(slim_elements_df)
+
+slim_elements_df['position'] = slim_elements_df.element_type.map(elements_types_df.set_index('id').singular_name)
+
+print(slim_elements_df.head())
