50ae8aaabd0859c91fa0de3ec4e4d903b13301b8
David Jin <davidjin2022@u.northwestern.edu>
2020-09-14 22:57:01 -0500

    Player gameweek data and wrote TODOs

--- data.py
+++ data.py
@@ -4,7 +4,9 @@ import os
 import pandas as pd
 
 # def load_data(data_path):
+
 # Column 'element_type' from players_raw.csv is actually the position, just as numeric code rather than a String
+# 1 = GK, 2 = DEF, 3 = MID, 4 = FWD
 
 players_path = 'Fantasy-Premier-League/data/2020-21/players_raw.csv'
 players_data = pd.read_csv(players_path)
@@ -16,17 +18,31 @@ players_data = players_data.to_numpy()
 print(players_data)
 
 teams_path = 'Fantasy-Premier-League/data/2020-21/teams.csv'
-team_data = pd.read_csv(teams_path)
-# Probably want goals for, goals against, and goal difference
-team_data = team_data.loc[:, ['id', 'name', 'played', 'win', 'draw', 'loss', 'points', 'position',
+teams_data = pd.read_csv(teams_path)
+# Probably want the following data: goals for, goals against, and goal difference
+teams_data = teams_data.loc[:, ['id', 'name', 'played', 'win', 'draw', 'loss', 'points', 'position',
                               'strength_attack_away', 'strength_attack_home', 'strength_defence_away',
                               'strength_defence_home', 'strength_overall_away', 'strength_overall_home']]
-team_data = team_data.to_numpy()
-print(team_data)
+teams_data = teams_data.to_numpy()
+print(teams_data)
+
+# The following is a test to obtain player gameweek (gw) data for 2019-20 (previous season) Aaron Cresswell
+# TODO: Corroborate this player data with the team the player plays for, which is the 'team' field in players_data
+player_gw_path = 'Fantasy-Premier-League/data/2019-20/players/Aaron_Cresswell_376/gw.csv'
+player_gw_data = pd.read_csv(player_gw_path)
+player_gw_data = player_gw_data.loc[:, ['assists', 'bonus', 'bps', 'clean_sheets', 'goals_conceded', 'goals_scored',
+                                        'minutes', 'opponent_team', 'own_goals', 'penalties_missed', 'penalties_saved',
+                                        'red_cards', 'saves', 'minutes', 'team_h_score', 'team_a_score',
+                                        'total_points', 'value']]
+player_gw_data = player_gw_data.to_numpy()
+print(player_gw_data)
+
 
 def get_player_gw_path(first_name, second_name, id):
+    # Note this is 2019-20 because there is no player data for the new season yet
     return 'Fantasy-Premier-League/data/2019-20/players/' + first_name + '_' + second_name + '_' + id + '/gw.csv'
 
+# TODO: Some things to think about here
 # Maybe for each metric (goals, assists, bonus; total), we can plot the data points on the y axis against strength on
 # the x. For defenders, the x axis would be opponent attacking strength. For attackers, the x axis would be opponent
 # defending strength. Find some way to incorporate home/away. Maybe use the difference between the strengths of the 2
